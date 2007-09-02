44be33e5e1703d9da1a2103fb545e7b7a2c54e80
nickmain <nickmain@e29addf9-c237-0410-9dab-b6fc98e26c49>
2007-09-02 15:49:18 +0000

    Fix remaining issues after ABC interface refactoring

--- JavaSWF-AVM2/src/com/anotherbigidea/flash/avm2/model/AVM2QName.java
+++ JavaSWF-AVM2/src/com/anotherbigidea/flash/avm2/model/AVM2QName.java
@@ -1,6 +1,7 @@
 package com.anotherbigidea.flash.avm2.model;
 
 import com.anotherbigidea.flash.avm2.MultiNameKind;
+import com.anotherbigidea.flash.avm2.NamespaceKind;
 
 /**
  * A name qualified by a namespace
@@ -16,4 +17,20 @@ public class AVM2QName extends AVM2Name {
     public AVM2QName( AVM2Namespace namespace, String name ) {
         super( MultiNameKind.Qname, namespace, name, null );
     }    
+    
+    /**
+     * @param qualifiedName a fully qualified name
+     */
+    public AVM2QName( String qualifiedName ) {
+        this( new AVM2Namespace( NamespaceKind.PackageNamespace, 
+                                 packagePrefix( qualifiedName )),
+              simpleName( qualifiedName ) );
+    }    
+    
+    /**
+     * Get the as a fully qualified name string
+     */
+    public String toQualString() {
+        return namespace.name + "." + name;
+    }
 }
