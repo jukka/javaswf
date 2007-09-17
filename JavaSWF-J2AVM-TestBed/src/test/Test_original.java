baa77c27a01ce3739e9b7130e88141f29157f0c6
nickmain <nickmain@e29addf9-c237-0410-9dab-b6fc98e26c49>
2007-09-17 13:43:55 +0000

    BACKUP - in the middle of a major refactoring arc

--- JavaSWF-J2AVM-TestBed/src/test/Test_original.java
+++ JavaSWF-J2AVM-TestBed/src/test/Test_original.java
@@ -6,39 +6,40 @@ import flash.text.StyleSheet;
 import flash.text.TextField;
 import flash.text.TextFieldAutoSize;
 
-public class Test extends MovieClip {
-    public Test() {
+public class Test_original extends MovieClip {
+
+    public Test_original() {
         drawTest();
     }
     
     public void drawTest() {
         Graphics g = getGraphics();
-    
+        
         for( int i = 0; i < 5; i++ ) {
-            drawSquare( g, 10 + i * 110, 10 );
-        }
+            drawSquare( g, 10 + i * 110, 10 );             
+        } 
         
         TextField field = new TextField();
         addChild( field );
         field.setX( 10 );
         field.setY( 120 );
-        field.setAutoSize( TextFieldAutoSize.LEFT );
-
+        field.setAutoSize( TextFieldAutoSize.LEFT ); 
+        
         StyleSheet style = new StyleSheet();
         style.parseCSS( ".foo { font-size: 24px }" );
         field.setStyleSheet( style );
-
-        field.setHtmlText( "<span class='foo'>agile sucks</span>" );
+        
+        field.setHtmlText( "<span class='foo'>drives me batty</span>" );        
     }
     
-    private void drawSquare(Graphics g, int x, int y) {
+    private void drawSquare( Graphics g, int x, int y ) {
         g.beginFill( 0x8080ff );
-        g.lineStyle( 2, 0xff0000 );
+        g.lineStyle( 2, 0xff0000 );        
         g.moveTo( x, y );
         g.lineTo( x + 100, y );
         g.lineTo( x + 100, y + 100 );
         g.lineTo( x, y + 100 );
         g.lineTo( x, y );
-        g.endFill();
+        g.endFill();        
     }
 }
